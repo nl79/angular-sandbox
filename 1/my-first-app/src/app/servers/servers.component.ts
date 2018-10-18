000dc2b73748aada501a490b95604b25a3fed352
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2018-10-17 21:29:24 -0400

    2 way binding and event examples

--- 1/my-first-app/src/app/servers/servers.component.ts
+++ 1/my-first-app/src/app/servers/servers.component.ts
@@ -4,14 +4,30 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  //template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was Created";
+  serverName = 'Test server';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created";
+  }
+  onUpdateServerName(event:any) {
+    console.log('event', event)
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
