9e49d29bfe462b797838b360d6fd8065254b6d33
Nazar <nl79@njit.edu>
2019-01-27 17:19:05 -0500

    Sample routing and module spec

--- 1/simple-routing/src/app/servers/edit-server/edit-server.component.ts
+++ 1/simple-routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -20,7 +21,9 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log('snapshot', this.route.snapshot.queryParams)
     console.log('fragment', this.route.snapshot.fragment)
-    this.route.queryParamMap.subscribe()
+    this.route.queryParamMap.subscribe( (queryParams: Params) => {
+      this.allowEdit = queryParams.params['allowEdit'] == 1 ? true : false;
+    } )
     this.route.fragment.subscribe()
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
