4b76316e9fc556cde2a557c179a51b625b965acd
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2019-03-17 20:03:02 -0400

    Simple forms and Initial Reactive forms example

--- 1/simple-forms/src/app/app.component.ts
+++ 1/simple-forms/src/app/app.component.ts
@@ -9,9 +9,39 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f')
   singupForm: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: '';
+
+  genders = ['male', 'female'];
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: "",
+    gender: ''
+  }
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.singupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ""
+    //   },
+    //   secret: 'pet',
+    //   // defaultQuestion: '',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.singupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(e, f: NgForm) {
@@ -22,5 +52,15 @@ export class AppComponent {
   // Local ref examle
   onSubmit() {
     console.log('singupform', this.singupForm);
+    this.user.username = this.singupForm.value.userData.username;
+    this.user.email = this.singupForm.value.userData.email;
+    this.user.secretQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+
+
+    this.submitted = true;
+
+    this.singupForm.reset()
   }
 }
